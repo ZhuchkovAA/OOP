b758feef7686ee7917efcff1f4730ddbee4df78f
ZhuchkovAA <112942160+ZhuchkovAA@users.noreply.github.com>
2023-09-20 19:06:58 +0300

    protectInfo 2laba

--- infoProtect/infoProtect/infoProtect_Zhuchkov.cpp
+++ infoProtect/infoProtect/infoProtect_Zhuchkov.cpp
@@ -1,10 +1,49 @@
-﻿#include <iostream>
+﻿#include <unordered_map>
+#include <algorithm>
+#include <iostream>
 #include <fstream>
+#include <vector>
 #include <string>
+#include <Windows.h>
 
 using namespace std;
 
-void vijener(bool isToShifr = true) {
+void vijener(string& inputFileName, string& keyWord, bool isToShifr, string outputFileName = "") {
+
+    char character, keySimbol;
+    int counter = 0;
+
+    ifstream inputFile(inputFileName, ios::binary);
+
+    if (outputFileName != "") {
+        ofstream outputFile(outputFileName, ios::binary);
+        outputFile << "";
+
+        while (inputFile.get(character)) {
+            keySimbol = keyWord[counter % keyWord.size()];
+            counter++;
+            isToShifr ? outputFile.put((character + keySimbol)) : outputFile.put(char(character - keySimbol));
+        }
+
+        cout << "\nУспешно!\n";
+
+        outputFile.close();
+    }
+    else {
+        while (inputFile.get(character)) {
+            keySimbol = keyWord[counter % keyWord.size()];
+            counter++;
+            if (isToShifr)
+                cout << unsigned char(character + keySimbol);
+            else
+                cout << unsigned char(character - keySimbol);
+        }
+    }
+
+    inputFile.close();
+}
+
+void vijenerOptions(bool isToShifr = true) {
     string fileNameAt, fileNameTo, keyWord;
 
     cout << "Введите имя файла(из которого считывать): ";
@@ -30,26 +69,151 @@ void vijener(bool isToShifr = true) {
         outputFile.close();
         return;
     }
-    outputFile << "";
 
     cout << "Введите кодовое слово: ";
     cin >> keyWord;
 
-    char character, keySimbol;
-    int counter = 0;
+    vijener(fileNameAt, keyWord, isToShifr, fileNameTo);
+}
 
-    while (inputFile.get(character)) {
-        keySimbol = keyWord[counter % keyWord.size()];
-        counter++;
-//        isToShifr ? outputFile.put(char((int(character) + int(keySimbol)) % 256)) :  outputFile.put(char((int(character) - int(keySimbol) + 256) % 256));
-        isToShifr ? outputFile.put((character + keySimbol)) : outputFile.put(char(character - keySimbol));
+void vijenerUpload(string& cipherFileName, string& key) {
+
+    basic_ifstream <unsigned char> inputFile;
+    ofstream outputFile;
+
+    string outputFileName;
+    cout << "Введите путь к файлу для записи: " << endl;
+    cin >> outputFileName;
+    
+    inputFile.open(cipherFileName, ios::binary);
+    outputFile.open(outputFileName, ios::binary);
+    
+    unsigned char inchar;
+    int keyIndex = 0;
+    
+    while (inputFile.get(inchar)) {
+        unsigned char outchar = inchar - key[keyIndex % key.length()];
+        // cout << outchar;
+        outputFile << outchar;
+        keyIndex++;
+    }
+    inputFile.close();
+    outputFile.close();
+}
+
+vector<int> getFrequency(vector<unsigned char> text) {
+    vector<int> frequency(256, 0);
+    for (auto simbol : text) {
+        frequency[simbol]++;
+    }
+    return frequency;
+}
+
+float getIC(vector<unsigned char>& text) {
+    vector<int> frequency;
+    frequency = getFrequency(text);
+
+    float IC = 0;
+    float sum = 0;
+
+    for (int count : frequency)
+        sum += count * (count - 1);
+
+    IC = sum / (text.size() * (text.size() - 1));
+    return IC;
+}
+
+int getKeyLength(vector<unsigned char> textCipher) {
+
+    int intStart, intEnd;
+    cout << "Введите диапазон символов:" << endl;
+    cin >> intStart; cin >> intEnd;
+
+    int keyLenght = 0;
+    double maxIC = 0, IC = 0;
+
+    for (int length = intStart; length <= intEnd; length++) {
+        vector<unsigned char> refText;
+
+        cout << "Анализ " << length << "-значного ключа..." << endl;
+        for (int i = 0; i < textCipher.size(); i++) {
+            if (i % length == 0)
+                refText.push_back(textCipher[i]);
+        }
+
+        IC = getIC(refText);
+        cout << "Полученый индекс соответсвия: " << IC << endl << endl;
+
+        if (IC > maxIC) {
+            maxIC = IC;
+            keyLenght = length;
+        }
     }
 
-    cout << "\nУспешно!\n";
+    cout << "\nПредполагаемая длина ключа: " << keyLenght << endl;
+    return keyLenght;
+}
+
+string getKey(vector<unsigned char> text, int keyLength) {
+    string key;
+    vector<unsigned char> group;
+
+    for (int i = 0; i < keyLength; i++) {
+
+        // Собираем символы, которые были зашифрованы с использованием i-го символа ключа
+        for (int j = i; j < text.size(); j += keyLength) {
+            group.push_back(text[j]);
+        }
+
+        // Выполняем частотный анализ для группы символов
+        vector<int> frequency = getFrequency(group);
+
+        // Находим наиболее вероятный символ ключа для данной позиции
+        char likelyKeyChar = 'A';
+        int maxFrequency = 0;
+
+        for (int j = 0; j < 256; j++) {
+            if (frequency[j] > maxFrequency) {
+                maxFrequency = frequency[j];
+                likelyKeyChar = static_cast<char>(j);
+            }
+        }
+
+        key += char(int(likelyKeyChar) + 48);
+        group.clear();
+    }
+
+    return key;
+}
+
+void ref() {
+
+    string cipherFileName, etalonFileName;
+
+    cout << "Введите имя файла с закодированным текстом: " << endl;
+    cin >> cipherFileName;
+    cout << "Введите имя файла с эталонным текстом: " << endl;
+    cin >> etalonFileName;
+
+    ifstream inputTextCipher(cipherFileName, ios::binary);
+    vector<unsigned char> textCipher((istreambuf_iterator<char>(inputTextCipher)), istreambuf_iterator<char>());
+
+    ifstream inputTextEtalon(etalonFileName, ios::binary);
+    vector<unsigned char> textEtalon((istreambuf_iterator<char>(inputTextEtalon)), istreambuf_iterator<char>());
+
+    string key;
+
+    int keyLength = getKeyLength(textCipher);
+    key = getKey(textCipher, keyLength);
+    cout << "Ключ: " << key << endl;
+    // vijener(cipherFileName, key, false);
+    vijenerUpload(cipherFileName, key);
 }
 
 int main()
 {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
     setlocale(LC_ALL, "Russian");
     system("chcp 1251");
 
@@ -62,14 +226,16 @@ int main()
 
         switch (state) {
         case 1: {
-            vijener();
+            vijenerOptions();
             break;
         }
         case 2: {
-            vijener(false);
+            vijenerOptions(false);
             break;
         }
-
+        case 3: 
+            ref();
+            break;
         default:
             break;
         }

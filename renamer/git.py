ae3410c6ca8fc47b0035a6dd0326417b0ba4734f
ZhuchkovAA <112942160+ZhuchkovAA@users.noreply.github.com>
2024-03-27 19:27:24 +0300

    test34

--- renamer/git.py
+++ renamer/git.py
@@ -5,29 +5,34 @@ import time
 import shutil
 import os
 
-def get_file_git():
-    url = "https://raw.githubusercontent.com/ZhuchkovAA/OOP/main/renamer/main.py"
+def get_file_git(file_name):
+    url = f"https://raw.githubusercontent.com/ZhuchkovAA/OOP/main/renamer/{file_name}"
     response = requests.get(url)
     if response.status_code == 200:
         return response.text
     else:
         return None
 
-def get_file_local(path_to):
-    with open(path_to['root'] + 'main.py', "r", encoding='utf8') as file:
+def get_file_local(path_to, file_name):
+    with open(path_to['root'] + file_name, "r", encoding='utf8') as file:
         return file.read()
     return None
 
 def is_equal_files(path_to):
-    git = get_file_git()
-    local = get_file_local(path_to)
+    files = ['main.py', 'git.py', 'additional.py']
+    files = ['main.py']
+    for file in files:
 
-    if git == None: 
-        print('Ошибка сети..')
-        return { 'success' : True }
+        git_file = get_file_git(file)
+        local = get_file_local(path_to, file)
 
-    if (hash(git) == hash(local)): return { 'success' : True }
-    return { 'success' : False,  'files' : {'git' : git, 'local' : local}}
+        if git_file == None: 
+            print('Ошибка сети..')
+            return { 'success' : True }
+
+        if (hash(git_file) == hash(local)): return { 'success' : True }
+
+    return { 'success' : False,  'files' : {'git' : git_file, 'local' : local}}
 
 def create_exe(path_to):
     subprocess.run(["pyinstaller", "--onefile", path_to['root'] + 'main.py'], check=True)
--- renamer/main.py
+++ renamer/main.py
@@ -7,68 +7,10 @@ import time
 import sys
 import subprocess
 
+from git import update_project
+from additional import animated_loading 
+
 # Для особо одарённых, я засунул всё в один файл тупо из-за того что exe нормально не компилился, а ебаться с этим желания ноль..
-
-def animated_loading():
-    chars = "/—\|" 
-    for char in chars:
-        sys.stdout.write('\r'+'loading...'+char)
-        time.sleep(.1)
-        sys.stdout.flush()
-
-def get_file_git():
-    url = "https://raw.githubusercontent.com/ZhuchkovAA/OOP/main/renamer/main.py"
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.text
-    else:
-        return None
-
-def get_file_local(path_to):
-    with open(path_to['root'] + 'main.py', "r", encoding='utf8') as file:
-        return file.read()
-    return None
-
-def is_equal_files(path_to):
-    git = get_file_git()
-    local = get_file_local(path_to)
-
-    if git == None: 
-        print('Ошибка сети..')
-        return { 'success' : True }
-
-    if (hash(git) == hash(local)): return { 'success' : True }
-    return { 'success' : False,  'files' : {'git' : git, 'local' : local}}
-
-def create_exe(path_to):
-    subprocess.run(["pyinstaller", "--onefile", path_to['root'] + 'main.py'], check=True)
-
-    try: 
-        shutil.rmtree("dist")
-        shutil.rmtree("build")
-        os.remove('main.spec')
-        shutil.rmtree(path_to['root'] + "build")
-        os.remove(path_to['root'] + 'main.spec')
-        shutil.rmtree(path_to['root'] + "__pycache__")
-    except:
-        pass
-
-def update_project(path_to):
-    print('Проверка обновлений...')
-    response = is_equal_files(path_to) 
-    if response['success']: return False
-
-    print('Установка обновлений...')
-
-    with open(path_to['root'] + 'main.py', 'w', encoding='utf8') as file:
-        file.write(response['files']['git'])
-        create_exe(path_to)
-
-    print('Требуется перезагрузка...')
-    time.sleep(1000)
-    return True
-
-
 
 def replace_name(name, names, root):
     path_ = {
